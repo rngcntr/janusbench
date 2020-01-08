be4d228a38822a827527e92251a9b03f23bb6551
Florian Grieskamp <florian.grieskamp@gdata.de>
2020-01-08 11:02:10 +0100

    Use managed session for gremlin clients
    Signed-off-by: Florian Grieskamp <florian.grieskamp@gdata.de>

--- services/gremlin-client/src/connect.groovy
+++ services/gremlin-client/src/connect.groovy
@@ -1,2 +1,11 @@
-:remote connect tinkerpop.server conf/remote.yaml
+:remote connect tinkerpop.server conf/remote.yaml session-managed
 :remote console
+
+while (true) {
+    try {
+        g;[] // wait for g to be available
+        break;
+    } catch (Exception e) {
+        Thread.sleep(1000);[]
+    }
+}
