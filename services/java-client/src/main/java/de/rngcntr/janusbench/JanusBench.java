985c79586775ff851cb3dfed53637e37e1baad35
Florian Grieskamp <florian.grieskamp@gdata.de>
2020-01-28 08:47:04 +0100

    Initialize a clusterd connection
    Signed-off-by: Florian Grieskamp <florian.grieskamp@gdata.de>

--- services/java-client/src/main/java/de/rngcntr/janusbench/JanusBench.java
+++ services/java-client/src/main/java/de/rngcntr/janusbench/JanusBench.java
@@ -1,11 +1,16 @@
 package de.rngcntr.janusbench;
 
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource;
-import org.apache.log4j.Logger;
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 
-import de.rngcntr.janusbench.benchmark.InsertVerticesBenchmark;
-import de.rngcntr.janusbench.benchmark.BenchmarkResult;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.driver.Client;
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.ResultSet;
+import org.apache.tinkerpop.gremlin.driver.exception.ResponseException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.log4j.Logger;
 
 public class JanusBench {
 
@@ -13,30 +18,61 @@ public class JanusBench {
     private static final Logger log = Logger.getLogger(JanusBench.class);
 
     private static GraphTraversalSource g;
+    private final String propertiesFileName;
+    private PropertiesConfiguration conf;
+    private Cluster cluster;
+    private Client client;
 
-    public static void main (String[] args) {
+    public JanusBench(final String propertiesFileName) {
+        this.propertiesFileName = propertiesFileName;
+    }
+
+    private void runBenchmarks() {
+        client.submit(g.addV());
+        ResultSet result = client.submit(g.V().count());
+        System.out.println(result.one().getInt());
+    }
+
+    private void openGraph() throws ConfigurationException {
+        log.info("Opening graph");
+        conf = new PropertiesConfiguration(propertiesFileName);
+
+        try {
+            cluster = Cluster.open(conf.getString("gremlin.remote.driver.clusterFile"));
+            client = cluster.connect();
+            client = client.alias("g");
+            g = traversal().withRemote(propertiesFileName);
+        } catch (final Exception ex) {
+            throw new ConfigurationException(ex);
+        }
+    }
+
+    private void closeGraph () throws ResponseException {
+        log.info("Closing graph");
+        client.close();
+    }
+
+    public static void main(final String[] args) {
         System.out.println("This is JanusBench v0.0.1");
 
-        initializeConnection(REMOTE_PROPERTIES);
-
-        log.info("Inserting Vertices");
-        InsertVerticesBenchmark ivb = new InsertVerticesBenchmark(g, 10);
-        ivb.run();
-        log.info(ivb.getResults());
+        final JanusBench jb = new JanusBench(REMOTE_PROPERTIES);
 
         try {
-            g.close();
-        } catch (Exception e) {
-            log.error("Unable to close connection to JanusGraph");
+            jb.openGraph();
+        } catch (final ConfigurationException cex) {
+            log.error("Unable to connect to graph");
+            log.error(cex);
         }
-    }
 
-    private static void initializeConnection (String propertiesFile) {
+        jb.runBenchmarks();
+
         try {
-            g = AnonymousTraversalSource.traversal().withRemote(propertiesFile);
-        } catch (Exception e) {
-            log.error("Unable open connection to JanusGraph");
+            jb.closeGraph();
+        } catch (final ResponseException rex) {
+            log.error("Unable to close graph");
+            log.error(rex);
         }
-    }
 
+        System.exit(0);
+    }
 }
