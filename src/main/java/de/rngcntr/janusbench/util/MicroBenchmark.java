9ece2e599d3b0418c342b2b4855c0d457c0b8a35
Florian Grieskamp <florian.grieskamp@gdata.de>
2020-04-08 12:16:07 +0200

    Enable output of MicroBenchmark result
    Signed-off-by: Florian Grieskamp <florian.grieskamp@gdata.de>

--- src/main/java/de/rngcntr/janusbench/util/MicroBenchmark.java
+++ src/main/java/de/rngcntr/janusbench/util/MicroBenchmark.java
@@ -1,11 +1,17 @@
 package de.rngcntr.janusbench.util;
 
 import de.rngcntr.janusbench.backend.Connection;
+import de.rngcntr.janusbench.util.BenchmarkProperty.Tracking;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeoutException;
+import org.apache.tinkerpop.gremlin.driver.Result;
+import org.apache.tinkerpop.gremlin.driver.ResultSet;
 
 /**
  * A MicroBenchmark is the simplest form of a benchmark, having neihter a buildUp nor a tearDown
@@ -49,7 +55,18 @@ public class MicroBenchmark extends Benchmark {
 
     @Override
     public void performAction() throws TimeoutException {
-        connection.submit(traversalString, parameters);
+        ResultSet resultSet = connection.submit(traversalString, parameters);
+        Iterator<Result> resultIterator = resultSet.iterator();
+        try {
+            Object result = resultIterator.next().getObject();
+            BenchmarkProperty resultProperty =
+                new BenchmarkProperty("result", (c) -> result);
+            collectBenchmarkProperty(resultProperty, Tracking.AFTER);
+        } catch (NoSuchElementException | CompletionException ignored) {
+            BenchmarkProperty resultProperty =
+                new BenchmarkProperty("result", (c) -> "null");
+            collectBenchmarkProperty(resultProperty, Tracking.AFTER);
+        }
     }
 
     @Override
